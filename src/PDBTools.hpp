fad7db476caec18b79fca82c9cb028e3edd3e4ad
yingyulou <yingyulou@vip.qq.com>
2021-09-02 12:42:14 +0800

    Refactor

---
+++ src/PDBTools.hpp
@@ -0,0 +1,16 @@
+/*
+    PDBTools.hpp
+    ============
+        PDBTools header.
+*/
+
+#include "Parser.hpp"
+#include "NotProtein.hpp"
+#include "NotAtom.hpp"
+#include "Protein.hpp"
+#include "Chain.hpp"
+#include "Residue.hpp"
+#include "Atom.hpp"
+#include "Util.hpp"
+#include "Math.hpp"
+#include "Constants.hpp"
